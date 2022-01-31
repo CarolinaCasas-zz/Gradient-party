8f086ef4cb84703f0adc6313d1f95d7c04da3a90
CarolinaCasas <casasc9@uabc.edu.mx>
2022-01-31 13:27:56 -0600

    allRoutes

--- src/ArticleComponents/BrowserCompability.js
+++ src/ArticleComponents/BrowserCompability.js
@@ -2,13 +2,29 @@
 const BrowserCompability = () => {
 
     return (
-        <section>
-            <h1>
+        <section className="infoSection">
+            <h2>
                 CSS Gradients Browser Compability
-            </h1>
+            </h2>
             <p>
-                table
+                Gradients are CSS elements of the image data type that show a transition between two or more colors. These transitions are shown as either linear or radial. Because they are of the image data type, gradients can be used anywhere an image might be. The most popular use for gradients would be in a background element.
+            </p><br />
+            <p>
+                To put it more relevantly, gradients are part of an extremely popular design trend that has been gaining popularity over the last several years. It seems that they have always been around in the background; although, some sources claim that the trend is “coming back”. Of course, its hard for something to come back if it never left, but well chalk that up to semantics.
+            </p><br />
+            <p>
+                Gradients are CSS elements of the image data type that show a transition between two or more colors. These transitions are shown as either linear or radial. Because they are of the image data type, gradients can be used anywhere an image might be. The most popular use for gradients would be in a background element.
+            </p><br />
+            <p>
+                To put it more relevantly, gradients are part of an extremely popular design trend that has been gaining popularity over the last several years. It seems that they have always been around in the background; although, some sources claim that the trend is “coming back”. Of course, its hard for something to come back if it never left, but well chalk that up to semantics.
+            </p><br />
+            <p>
+                Gradients are CSS elements of the image data type that show a transition between two or more colors. These transitions are shown as either linear or radial. Because they are of the image data type, gradients can be used anywhere an image might be. The most popular use for gradients would be in a background element.
+            </p><br />
+            <p>
+                To put it more relevantly, gradients are part of an extremely popular design trend that has been gaining popularity over the last several years. It seems that they have always been around in the background; although, some sources claim that the trend is “coming back”. Of course, its hard for something to come back if it never left, but well chalk that up to semantics.
             </p>
+            <hr/>
         </section>
     )
 
